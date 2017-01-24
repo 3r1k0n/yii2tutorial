58a391db835e0e779cb2956ac843c307bc9992ad
Erik Percic <3r1k0n@gmail.com>
2017-01-24 09:02:49 +0100

    Lesson 23

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -15,6 +15,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'assets/js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- backend/views/branches/index.php
+++ backend/views/branches/index.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\bootstrap\Modal;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\BranchesSearch */
@@ -17,8 +19,23 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Branches', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::button('Create Branches', ['value'=>Url::to('index.php?r=branches/create'),'class' => 'btn btn-success','id'=>'modalButton']) ?>
     </p>
+
+    <?php
+        Modal::begin([
+                'header'=>'<h4>Create a branch</h4>',
+                'id'=>'modal',
+                'size'=>'modal-lg',
+                'options'=>[
+                    'tabindex' => false
+                ]
+            ]);
+
+        echo "<div id='modalContent'></div>";
+
+        Modal::end();
+    ?>
 
     <?php Pjax::begin(); ?>
 
